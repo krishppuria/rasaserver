5688633967220300f4cd236e7f2ba5987ac9ec21
chetan520 <chetanrox520@gmail.com>
2019-01-10 17:15:06 +0530

    update

--- flask1.py
+++ flask1.py
@@ -1,14 +1,14 @@
 
 
 from flask import Flask,render_template,request
-from playbot import run
+import playbot
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
     name="hello"
-    x=run(name)
+    x=playbot.run(name)
     return render_template('page.html',data=x)
 #if __name__ == '__main__':
     #app.run(debug=True)
