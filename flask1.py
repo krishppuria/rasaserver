91171cb43d78d0f7ca5cd0bbad5db05859855bce
chetan520 <chetanrox520@gmail.com>
2019-01-10 18:05:33 +0530

    update

--- flask1.py
+++ flask1.py
@@ -7,8 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    name="hello"
-    x=name
+    x=playbot.run("hello")
     return render_template('page.html',data=x)
 if __name__ == '__main__':
     app.run(debug=True)
