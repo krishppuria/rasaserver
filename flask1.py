e50be8deaafd16cae4fd6b152d9173ad0f440a49
chetan520 <chetanrox520@gmail.com>
2019-01-10 16:30:57 +0530

    update

--- flask1.py
+++ flask1.py
@@ -8,16 +8,10 @@ app = Flask(__name__)
 @app.route('/', methods=["GET","POST"])
 def welcome():
     if request.method == "POST":
-	name=request.args.get('user')
+	name=request.args.post('user')
         x=run(name)
         return render_template('page.html',data=x)
     return render_template('loggin.html')
-
-@app.route('/user')
-def data():
-    name=request.args.get('user')
-    x=run(name)
-    return render_template('page.html',data=x)
 #if __name__ == '__main__':
     #app.run(debug=True)
 
