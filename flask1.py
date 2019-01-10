4ee43757c9b75a17adcf1c9e8aaba0b20a29b40c
krishna <krishna@localhost.localdomain>
2019-01-10 10:06:49 -0800

    update

--- flask1.py
+++ flask1.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    x=playbot.run("elegibility in bca")
+    x=playbot.run("hello")
     return render_template('page.html',data=x)
 if __name__ == '__main__':
     app.run(debug=True)
