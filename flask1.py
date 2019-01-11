35973b8816e166714fa7fca722d3c77f560ed99b
chetan520 <chetanrox520@gmail.com>
2019-01-11 12:39:18 +0530

    update

--- flask1.py
+++ flask1.py
@@ -1,13 +1,13 @@
-
-
 from flask import Flask,render_template,request
-import playbot
-
+from playbot import run
 app = Flask(__name__)
-
 @app.route('/')
 def welcome():
-    x=playbot.run("hello")
+    return render_template('loggin.html')
+@app.route('/user')
+def data():
+    name=request.args.get('user')
+    x=run(name)
     return render_template('page.html',data=x)
 if __name__ == '__main__':
     app.run(debug=True)
